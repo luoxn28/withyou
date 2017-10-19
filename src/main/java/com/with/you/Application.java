d484efcdb83935133c765c75dc8231fa7574f95d
luoxn28 <luoxn28@163.com>
2017-10-19 18:21:47 +0800

    初始化后端工程

---
+++ src/main/java/com/with/you/Application.java
@@ -0,0 +1,23 @@
+package com.with.you;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("SpringBoot start...");
+    }
+
+}
