63cdcca9824da799eec5b60420f574c56a8990bf
luoxn28 <luoxn28@163.com>
2017-10-21 19:38:22 +0800

    完善记事页面

--- src/pages/Note.vue
+++ src/pages/Note.vue
@@ -8,27 +8,33 @@
       <x-textarea name="description" v-model="note.text" placeholder="content" :max="1000" autosize></x-textarea>
     </group>
 
-    <flexbox class="sumit-box">
-      <flexbox-item>
-        <x-button type="warn" @click.native="clean">重置</x-button>
-      </flexbox-item>
-      <flexbox-item>
-        <x-button type="primary" @click.native="submit">提交</x-button>
-      </flexbox-item>
-    </flexbox>
+    <box gap="10px 10px">
+      <flexbox>
+        <flexbox-item>
+          <x-button type="warn" @click.native="clean(true)">重置</x-button>
+        </flexbox-item>
+        <flexbox-item>
+          <x-button type="primary" @click.native="submit">提交</x-button>
+        </flexbox-item>
+      </flexbox>
+    </box>
 
   </div>
 </template>
 
 <script>
-  import { XTextarea, Group, XInput, XButton } from 'vux'
+  import Vue from 'vue'
+  import { XTextarea, Group, Box, XInput, XButton } from 'vux'
   import { Flexbox, FlexboxItem } from 'vux'
+  import { Toast, ToastPlugin } from 'vux'
   import AppMenu from '../components/Menu'
   import AppFooter from '../components/Footer'
 
+  Vue.use(ToastPlugin)
+
   export default {
     components: {
-      XTextarea, Group, XInput, XButton,
+      XTextarea, Group, Box, XInput, XButton,
       Flexbox, FlexboxItem,
       AppMenu, AppFooter
     },
@@ -42,20 +48,48 @@
     },
     methods: {
       submit() {
-        console.info(this.note)
-
-        this.clean()
+        if (this.checkData()) {
+          this.$http.post("/api/note", {
+            title: this.note.title,
+            text: this.note.text
+          }, {emulateJSON:true}).then((response) => {
+            this.$vux.toast.show({
+              text: '提交成功'
+            });
+            this.clean(false);
+          }).then((response) => {
+            // TODO
+          });
+        } else {
+          this.$vux.toast.show({
+            type: 'cancel',
+            text: '数据不能为空'
+          });
+        }
       },
-      clean() {
-        this.note.title = ''
-        this.note.text = ''
+      clean(show) {
+        this.note.title = '';
+        this.note.text = '';
+
+        if (show) {
+          this.$vux.toast.show({
+            text: '重置成功'
+          });
+        }
+      },
+      checkData() {
+        if ((this.note.title === null) || (this.note.title === '')) {
+          return false;
+        }
+        if ((this.note.text === null) || (this.note.text === '')) {
+          return false;
+        }
+
+        return true;
       }
     }
   }
 </script>
 
 <style>
-  .sumit-box {
-    margin-top: 10px;
-  }
 </style>
